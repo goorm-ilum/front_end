7048b00fe54385b7d194a7863eeacfc3300bb852
조유경 <yugyeongjo1215@gmail.com>
2025-07-28 23:31:08 +0900

    modify : layout

--- src/common/BasicLayout.jsx
+++ src/common/BasicLayout.jsx
@@ -1,20 +1,32 @@
-import Header from './Header';
-import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet, Link } from "react-router-dom";  
 
-// 이 값을 true로 하면 전체 폭, false로 하면 고정 폭(1200px)
 const isFullWidth = true;
 
 const BasicLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
-      <main className="flex-1 flex justify-center px-4 py-8">
-        <div className={isFullWidth ? "w-full min-h-[400px] flex flex-col justify-center" : "w-full max-w-[1200px] mx-auto min-h-[600px] flex flex-col justify-center"}>
+    <div className="min-h-screen overflow-x-hidden bg-gray-50">
+      {/* Header */}
+      <div className="px-4">
+        <div className="w-[1200px] mx-auto">
+          <Header />
+        </div>
+      </div>
+
+      {/* Main */}
+      <main className="px-4 py-8">
+        <div className="w-[1200px] mx-auto">
           <Outlet />
         </div>
       </main>
-      <Footer />
+
+      {/* Footer */}
+      <div className="px-4">
+        <div className="w-[1200px] mx-auto">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
