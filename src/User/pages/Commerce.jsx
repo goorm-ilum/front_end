cb2a8c4aa6bba45dfcc1a6abb0d823aac3f81b86
조유경 <yugyeongjo1215@gmail.com>
2025-07-29 15:26:21 +0900

    add

--- src/User/pages/Commerce.jsx
+++ src/User/pages/Commerce.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 const Commerce = () => {
   return (
     <div className="p-4">
-      <Outlet /> {/* 자식 라우트들이 여기 들어옴 */}
+      <Outlet />
     </div>
   );
 };
